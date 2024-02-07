99dd05988ee379292bb9c081fa22ddc5abd16f20
ivy13534 <andhika080305@gmail.com>
2024-02-07 14:02:01 +0700

    Marai Rusak Chrome

--- js/script.js
+++ js/script.js
@@ -1,17 +1,110 @@
-alert("Hello World");
-//console.log("Hello World from console");
+////alert("Hello World");
+////console.log("Hello World from console");
+//
+////Komentar
+//
+///*
+//komentar
+//*/
+//
+////Tipe Data
+///*
+//console.log("String");
+//console.log(2);
+//let nama = "Budi";
+//const umur = 29;
+//nama = "agus";
+//console.log(nama);
+//console.log(umur);
+//*/
+////Operator matematika
+//console.log(2+5);
+//console.log(5-2);
+//console.log(3*2);
+//console.log(2**4);
+//console.log(6/3);
+//console.log(6%5)
+//
+////Penjumlahan number
+//let angka1 = 20;
+//let angka2 = 40;
+//console.log(angka1 + angka2);
+//
+////Penjumlahan string
+//let namadepan = "Yohanes";
+//let namatengah = "Andhika";
+//let namabelakang = "Pamungkas";
+//console.log(namadepan + " " + namatengah + " " + namabelakang);
+//
+//let kata = "ganteng";
+//let angka = 10;
+//console.log(kata + angka); //Penjumlahan string dan number
+//
+////math short-hand
+//let result = 5;
+//result += 10;
+//console.log(result);
+//
+////operator unary
+////let x = 10;
+//x++;
+//console.log(x);
+//
+//console.log(8>7);
+//console.log(8<7);
+//console.log(7==7);
+//console.log(10 >= 10);
+//console.log(20 <=  20);
+//console.log(10 != 20);
+//
+//alert("Ini adalah alert");
+//confirm("Apakah anda yakin?");
+//prompt("Masukkan nama anda!");
+//
+////operator logika
+////salah satu false hasilnya false
+//
+//If statement
+//let youlikemeat = true;
+//if (youlikemeat) {
+//    console.log("Anda suka makan daging");
+//} else{
+//    console.log("Anda tidak suka makan daging");
+//}
+//
+////else if
+//let umur = 17;
+//if (umur < 17) {
+//    console.log("Anda belum bisa membuat KTP");
+//} else if (umur >= 17) {
+//    console.log("Anda sudah dapat membuat KTP");
+//}
+//
+////for loop
+//for (let kue = 5; kue >=1; kue--) {
+//    console.log("Kue sudah dibagikan, kue tersisa" + " " +kue);
+//}
+//function perkalian(angka1, angka2){
+//    let hasil = angka1 * angka2;
+//    console.log("hasil perkalian dari " +angka1+ " dan " +angka2+" adalah " + hasil)
+//}
+//perkalian(10, 50);
+//
+//let buku = 5000;
+//function hargaproduk() {
+//    let pulpen = 3000
+//}
+//console.log(pulpen);
 
-//Komentar
+//Array
+//let motor = ["Toyota", "Supra", "Honda"];
+//console.log(motor);
 
-/*
-komentar
-*/
-
-//Tipe Data
-console.log("String");
-console.log(2);
-let nama = "Budi";
-const umur = 29;
-nama = "agus";
-console.log(nama);
-console.log(umur);
+let mobil = {
+    nama : "BMW",
+    model : "X5",
+    Berat : "100",
+    Tahun : "2000",
+}
+console.log(mobil);
+console.log(mobil.nama);
