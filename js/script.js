2f2734ba68c48270606a4844fbb348f2ee610693
ivy13534 <andhika080305@gmail.com>
2024-02-07 12:21:49 +0700

    Pembelajaran Pertama

---
+++ js/script.js
@@ -0,0 +1,17 @@
+alert("Hello World");
+//console.log("Hello World from console");
+
+//Komentar
+
+/*
+komentar
+*/
+
+//Tipe Data
+console.log("String");
+console.log(2);
+let nama = "Budi";
+const umur = 29;
+nama = "agus";
+console.log(nama);
+console.log(umur);
